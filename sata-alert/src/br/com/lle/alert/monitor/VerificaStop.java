f72bbce57b6da1253d45c22a6a3ab53b6b22ab3c
lyntonbr9 <lyntonbr9@gmail.com>
2015-08-30 06:38:23 -0300

    - versao 1.10 no playstore

--- sata-alert/src/br/com/lle/alert/monitor/VerificaStop.java
+++ sata-alert/src/br/com/lle/alert/monitor/VerificaStop.java
@@ -1,5 +1,7 @@
 package br.com.lle.alert.monitor;
 
+import static br.com.lle.sata.util.LogUtil.log;
+
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -16,14 +18,14 @@ public class VerificaStop
 		for (StopAtivo sa : as.getStopsAtivo()) {
 			if (sa.isHabilitado()) {
 				String strCotacao = bs.getCotacao(sa.getCodigoAtivo()).replaceAll(",",".");
-				System.out.println("\nCOTACAO " + sa.getCodigoAtivo() + ": " + strCotacao);
+				log("\nCOTACAO " + sa.getCodigoAtivo() + ": " + strCotacao);
 				BigDecimal cotacao = new BigDecimal(strCotacao);
 				// se a cotacao estiver menor do que o preco de disparo
 				if (cotacao.compareTo(sa.getMinDisparo().getPreco()) == -1 
 								&& sa.getMinDisparo().getQtdNotificacoes() < StopAtivo.MAX_NUM_NOTIFIC) {
 					// cria o alerta
 					String msg = sa.getCodigoAtivo() + " SL: " + sa.getMinDisparo().getPreco() + " PR: " + cotacao;
-					System.out.println("Alerta " + msg);
+					log("Alerta " + msg);
 					sa.getMinDisparo().setNotificar(true);
 					sa.getMinDisparo().setMensagem(msg);
 					sa.getMinDisparo().setQtdNotificacoes(sa.getMinDisparo().getQtdNotificacoes() + 1);
@@ -32,7 +34,7 @@ public class VerificaStop
 								&& sa.getMaxDisparo().getQtdNotificacoes() < StopAtivo.MAX_NUM_NOTIFIC) {
 					// cria o alerta
 					String msg = sa.getCodigoAtivo() + " SH: " + sa.getMaxDisparo().getPreco() + " PR: " + cotacao;
-					System.out.println("Alerta " + msg);
+					log("Alerta " + msg);
 					sa.getMaxDisparo().setNotificar(true);
 					sa.getMaxDisparo().setMensagem(msg);
 					sa.getMaxDisparo().setQtdNotificacoes(sa.getMaxDisparo().getQtdNotificacoes() + 1);
